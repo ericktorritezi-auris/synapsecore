6a871d47120719c2a4f26a0075f19b8bb7af5cfd
ericktorritezi-auris <erick.torritezi@gmail.com>
2026-05-15 22:57:46 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { runMigrations } = require('./db/migrations');
 
 const app     = express();
 const PORT    = process.env.PORT || 3000;
-const VERSION = '3.1.7';
+const VERSION = '3.1.8';
 
 // ── MIDDLEWARE ──
 app.use(cors());
